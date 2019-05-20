aff386c362849ad64989fa34bd50d20d1cc1c65c
sigevsky <nick.sigevsky@gmail.com>
2019-05-20 20:13:07 +0300

    init

---
+++ logistic.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class LogisticLayer:
+    def __init__(self, dim):
+        self.W = np.random.rand(1, dim)
+        self.b = np.random.random()
+        self._sig_f = None
+        self._x_f = None
+
+    @staticmethod
+    def sig(x):
+        return 1. / (1. + np.power(np.e, -x))
+
+    def __call__(self, x):
+        self._sig_f = self.sig(np.dot(self.W, x) + self.b)
+        self._x_f = x
+        return self._sig_f
+
+    def backward(self, er):
+        dsig = self._sig_f * (1 - self._sig_f)
+        e = er * dsig  # shape = (1, m)
+        dw = 1 / len(e) * np.sum(np.dot(e, self._x_f.T), axis=0)
+        db = 1 / len(e) * np.sum(e)
+        return dw, db
---
+++ losses.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class MSE:
+    def __init__(self):
+        self._y_hat = None
+        self._y = None
+
+    def __call__(self, y, y_hat):
+        assert y_hat.shape == y.shape
+        self._y_hat = y_hat
+        self._y = y
+        return 1 / (2 * y.size) * np.sum(np.power((y - y_hat), 2))
+
+    def backwards(self):
+        return 1 / self._y_hat.size * (self._y_hat - self._y)
+
+
+class CrossEntropyLoss:
+    def __init__(self):
+        self._y_hat = None
+        self._y = None
+
+    def __call__(self, y, y_hat):
+        assert y_hat.shape == y.shape
+        self._y_hat = y_hat
+        self._y = y
+        return -1 / y.size * np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+
+    def backwards(self):
+        return -1 / self._y_hat.size * (self._y / self._y_hat - (1 - self._y) / (1 - self._y_hat))
